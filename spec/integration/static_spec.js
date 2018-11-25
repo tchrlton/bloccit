b8da2abc516cbe20b6468db46767a9a51517dcb2
Thomas Charlton <tchrlton1@gmail.com>
2018-11-24 23:16:53 -0800

    added /marco route for assignment

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server.js");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -9,6 +10,16 @@ describe("routes : static", () => {
         it("should return status code 200", (done) => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
+    });
+
+    describe("GET /marco", () => {
+        it("should return status code and the body should contain 'polo'", (done) => {
+            request.get(marco, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toBe("polo");
                 done();
             });
         });
--- src/routes/static.js
+++ src/routes/static.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/marco", (req, res, next) => {
+    res.send("polo");
+});
+
 router.get("/", (req, res, next) => {
     res.send("Welcome to Bloccit");
 });
