b9e72e85c0d59bf94d27f0c3978e0a0deb4b318e
Thomas Charlton <tchrlton1@gmail.com>
2018-11-25 17:24:29 -0800

    added about page route, handler, and partials

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server.js");
 const base = "http://localhost:3000/";
+const about = "http://localhost:3000/about";
 
 describe("routes : static", () => {
 
@@ -10,6 +11,17 @@ describe("routes : static", () => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
                 expect(body).toContain("Welcome to Bloccit");
+                done();
+            });
+        });
+    });
+
+    describe("GET /about", () => {
+
+        it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
+            request.get(about, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toContain("About Us");
                 done();
             });
         });
